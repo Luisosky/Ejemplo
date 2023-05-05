6a65d9212e90d3291d191575157307204073fe12
Lamsu1505 <131729688+Lamsu1505@users.noreply.github.com>
2023-05-05 16:03:15 -0500

    Cree la clase pacho

---
+++ src/ejemplogithub/Pacho.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejemplogithub;
+
+/**
+ *
+ * @author perez
+ */
+public class Pacho {
+    
+    private int polas;
+    public Pacho(int polas)
+    {
+        this.polas = polas;
+    }
+    
+    public void tomarPolas(int polas)
+    {
+        System.out.println("Tinti se toma " + polas + " diarias");
+    }
+    
+}

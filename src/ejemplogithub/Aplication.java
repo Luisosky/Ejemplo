4138adfa78b6e169705d23be907c4557f1bd55e4
andresLamsu <perezmartinezandres0@gmail.com>
2024-11-13 19:59:22 -0500

    Login hecho

--- src/ejemplogithub/Aplication.java
+++ src/ejemplogithub/Aplication.java
@@ -2,17 +2,18 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+
+
 package ejemplogithub;
 
 /**
  *
  * @author perez
  */
-public class TintiGod {
-    
-    public static void main(String args[])
-    {
-        System.out.println("Tintotrago es god");
-    }
+public class Aplication {
+    public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Panel_Tabla_Venta_Moto= new tabla();
+	}
     
 }
--- src/ejemplogithub/Ejemplo2.java
+++
@@ -1,9 +0,0 @@
-package ejemplogithub;
-
-public class Ejemplo2 {
-
-    String nombre="Eduard hizo este cambio";
-  
-  
-
-}
